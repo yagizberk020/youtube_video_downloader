320388a56f439095a3e161f133a0599fc202dad5
yagizberk020 <sezeryagizalp12@gmail.com>
2022-03-19 12:28:11 +0300

    youtube downloader
    initial interface completed

---
+++ README.md
@@ -0,0 +1,5 @@
+* user enters video url to download
+* user enters path to save the video to
+  * if no path provided save to a default directry
+* download the video
+* inform the user about the results
---
+++ main.py
@@ -0,0 +1,25 @@
+def download_video(url, path):
+    return True
+
+
+default_path = "videos"
+
+# user enters video url to download
+url = input("Please enter youtube url: ")
+
+# user enters path to save the video to
+path = input("please select path to download: ")
+
+# if no path provided save to a default directry
+if len(path) == 0:
+    path = default_path
+    print("using the default path")
+
+# download the video
+status = download_video(url, path)
+
+# inform the user about the result
+if status == True:
+    print("Video downloaded to",path)
+else:
+    print("Sorry we couldn't download the video")
