bbda9a02cecf93257fb9f2a6c5f3467d2d52b810
yagizberk020 <sezeryagizalp12@gmail.com>
2022-03-19 12:43:55 +0300

    getpath function added
    refactored main function and added a new getpath function

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ def download_video(url, path):
     return True
 
 
-default_path = "videos"
-def main():
-    # user enters video url to download
-    url = input("Please enter youtube url: ")
-
+def getpath():
+    # get path from user and create if not exist
     # user enters path to save the video to
     path = input("please select path to download: ")
 
@@ -14,6 +11,16 @@ def main():
     if len(path) == 0:
         path = default_path
         print("using the default path")
+    return path
+
+
+default_path = "videos"
+def main():
+    # user enters video url to download
+    url = input("Please enter youtube url: ")
+
+    path = getpath()
+
 
     # download the video
     status = download_video(url, path)
