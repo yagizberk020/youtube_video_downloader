feca0208e0f4f64b7e5034e9ed13da1db7aab9cc
yagizberk020 <sezeryagizalp12@gmail.com>
2022-03-19 12:36:24 +0300

    included a main fanction

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ def download_video(url, path):
 
 
 default_path = "videos"
+def main():
+    # user enters video url to download
+    url = input("Please enter youtube url: ")
 
-# user enters video url to download
-url = input("Please enter youtube url: ")
+    # user enters path to save the video to
+    path = input("please select path to download: ")
 
-# user enters path to save the video to
-path = input("please select path to download: ")
+    # if no path provided save to a default directry
+    if len(path) == 0:
+        path = default_path
+        print("using the default path")
 
-# if no path provided save to a default directry
-if len(path) == 0:
-    path = default_path
-    print("using the default path")
+    # download the video
+    status = download_video(url, path)
 
-# download the video
-status = download_video(url, path)
+    # inform the user about the result
+    if status == True:
+        print("Video downloaded to",path)
+    else:
+        print("Sorry we couldn't download the video")
 
-# inform the user about the result
-if status == True:
-    print("Video downloaded to",path)
-else:
-    print("Sorry we couldn't download the video")
+if __name__ == '__main__':
+    main()
